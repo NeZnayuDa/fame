1f1c9e68cd74b100b98969f98a35348b2e3df925
NeZnayuDA <rogzi0011@gmail.com>
2026-02-19 11:13:37 +0500

    sdfsdf

---
+++ app.js
@@ -0,0 +1,54 @@
+let ff = [
+    {
+        image: "ss.png",
+        text: "New",
+        module: "Atarashi Series®"
+    },
+    {
+        image: "rr.png",
+        text: "Featured",
+        module: "Kensho Series®"
+    },
+    {
+        image: "gg.png",
+        text: "Handmade",
+        module: "Nairo Series®"
+    }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nijeLegoDiv = document.querySelector('.nije_lego');
+
+    const item = ff[0]; // только одно фото
+
+    const div = document.createElement('div');
+    div.classList.add('item');
+
+    const img = document.createElement('img');
+    img.src = item.image;
+    img.alt = item.text;
+
+    // размеры
+    img.style.width = "736px";
+    img.style.height = "700px";
+    img.style.objectFit = "cover"; // чтобы не растягивалось криво
+
+    const text = document.createElement('p');
+    text.textContent = `${item.text} - ${item.module}`;
+
+    div.appendChild(img);
+    nijeLegoDiv.appendChild(div);
+});
+let container = document.querySelector('.fa');
+ff.forEach(item => {
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+        <img src="${item.image}" alt="${item.text}">
+        <h3>${item.text}</h3>
+        <p>${item.module}</p>
+    `;
+
+    container.appendChild(card);
+});
